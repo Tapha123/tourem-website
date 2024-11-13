89315f8756364c8ac56bb0037adfb43833afa0ec
Tapha123 <toure.moostapha@gmail.com>
2024-11-13 17:00:01 -0500

    Commit my website

--- tourem-website/My-Website-MT.R
+++ tourem-website/My-Website-MT.R
@@ -1,3 +1,11 @@
-# install.packages("usethis")
+# To use Git/GitHub now with this existing site that you can serve locally
+install.packages("usethis")
 usethis::use_git()
 usethis::use_github() # requires a GitHub PAT
+
+# To update your theme, inside your website project
+blogdown::install_theme(theme = "hugo-apero/hugo-apero",
+                        update_config = FALSE, 
+                        force = TRUE)
+
+blogdown::serve_site()
